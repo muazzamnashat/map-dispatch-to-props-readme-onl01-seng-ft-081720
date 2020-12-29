d632f1b5db8898e0e19a97ef510855128b54626c
muazzamnashat <muazzamhc.nashat@gmail.com>
2020-12-29 04:58:53 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   }
 
   render() {
+    
     return (
       <div className="App">
         <button onClick={(event) => this.handleOnClick(event)}>
@@ -27,4 +28,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) =>{
+  return { 
+    addItem: () => dispatch(addItem())
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
